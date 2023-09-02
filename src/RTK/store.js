3cd893a8420b3b60225a628e65cc3b428c6ba760
Fuck the Error! <74085442+Kaung-Myat-Soe@users.noreply.github.com>
2023-09-02 18:41:44 +0630

    Slice add

--- src/RTK/store.js
+++ src/RTK/store.js
@@ -1,13 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authApi } from './API/Auth'
+import AuthSlice from './Services/AuthSlice'
+import CartSlice from './Services/CartSlice'
+import { fakeAuthApi } from './API/FakeAuth'
 
 
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [fakeAuthApi.reducerPath]: fakeAuthApi.reducer,
+
+
+    AuthSlice : AuthSlice,
+    CartSlice : CartSlice,
+
 
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware,fakeAuthApi.middleware),
 })
