3e49d8a5590f9902094021a44dd4e12305f0e7ce
Fuck the Error! <74085442+Kaung-Myat-Soe@users.noreply.github.com>
2023-09-02 18:45:25 +0630

    Add To Cart With Qty

--- src/RTK/Services/CartSlice.js
+++ src/RTK/Services/CartSlice.js
@@ -7,8 +7,16 @@ const CartSlice = createSlice({
         cart: []
         },
     reducers: {
+        
       AddToCart:(state, {payload})=> {
-        state.cart = [...state.cart,payload]
+        const productExit = cart.find((item)=>item.id === product.id)
+    if(productExit){
+        state.cart =cart.map((item)=>
+      (item.id === payload.id? { ... productExit, qty:productExit.qty + 1 
+      } : item ))
+
+    }
+        
         Cookies.set('Cart', JSON.stringify(state))
       },
     },
